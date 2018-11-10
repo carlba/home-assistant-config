ed1823d60d4a16035f0e1248e53c57cf2042edde
Carl Bäckström <genzorg@gmail.com>
2018-11-10 22:22:10 +0100

    Added config_updater and script to install it

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir custom_components
+(cd custom_components && curl -O 'https://raw.githubusercontent.com/custom-components/custom_updater/master/custom_components/custom_updater.py')
+
+
+
