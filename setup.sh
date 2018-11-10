c94d2ff32b020b2df7f2e734afaddccd267b0b02
Carl Bäckström <genzorg@gmail.com>
2018-11-10 23:21:46 +0100

    Configured custom_updater, tracker_card and added mini-media-player

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir custom_components
 (cd custom_components && curl -O 'https://raw.githubusercontent.com/custom-components/custom_updater/master/custom_components/custom_updater.py')
-
-
+(cd www && curl -O 'https://raw.githubusercontent.com/custom-cards/tracker-card/master/tracker-card.js')
+(cd www && curl -O 'https://github.com/kalkih/mini-media-player/releases/download/v0.9.2/mini-media-player-bundle.js')
 
