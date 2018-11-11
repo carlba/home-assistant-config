bbff7eceeeb5abe107ae0e848fa77f969c7c2bf1
Carl Bäckström <genzorg@gmail.com>
2018-11-12 00:53:42 +0100

    Restructured lovelace ui into swip cards and created new tab tracker.

--- setup.sh
+++ setup.sh
@@ -7,3 +7,12 @@
 (cd www && curl -OL 'https://raw.githubusercontent.com/thomasloven/lovelace-slider-entity-row/master/slider-entity-row.js')
 # https://github.com/custom-cards/light-entity-row
 (cd www && curl -OL 'https://raw.githubusercontent.com/custom-cards/light-entity-row/master/light-entity-row.js')
+# https://github.com/custom-cards/circle-sensor-card
+(cd www && curl -OL 'https://raw.githubusercontent.com/custom-cards/circle-sensor-card/master/circle-sensor-card.js')
+[[ ! -d 'www/custom-lovelace/swipe-card' ]] && mkdir -p www/custom-lovelace/swipe-card
+(cd www/custom-lovelace/swipe-card && curl -OL 'https://raw.githubusercontent.com/bramkragten/custom-ui/master/swipe-card/swipe-card.js')
+[[ ! -d 'www/custom-lovelace/swipe-card/js' ]] && mkdir -p www/custom-lovelace/swipe-card/js
+(cd www/custom-lovelace/swipe-card/js && curl -OL 'https://raw.githubusercontent.com/bramkragten/custom-ui/master/swipe-card/js/swiper.min.js')
+[[ ! -d 'www/custom-lovelace/swipe-card/css' ]] && mkdir -p www/custom-lovelace/swipe-card/css
+(cd www/custom-lovelace/swipe-card/css && curl -OL 'https://raw.githubusercontent.com/bramkragten/custom-ui/master/swipe-card/css/swiper.min.css')
+cd ..
