f3c4c599a8450d876b3aff632ac98b010b31213b
Carl Bäckström <genzorg@gmail.com>
2018-11-11 11:22:22 +0100

    Trying to get lovelace to look good

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-mkdir custom_components
+[[ ! -d 'custom_components' ]] && mkdir custom_components
+[[ ! -d 'www' ]] && mkdir www
 (cd custom_components && curl -O 'https://raw.githubusercontent.com/custom-components/custom_updater/master/custom_components/custom_updater.py')
 (cd www && curl -O 'https://raw.githubusercontent.com/custom-cards/tracker-card/master/tracker-card.js')
-(cd www && curl -O 'https://github.com/kalkih/mini-media-player/releases/download/v0.9.2/mini-media-player-bundle.js')
+(cd www && curl -OL 'https://github.com/kalkih/mini-media-player/releases/download/v0.9.2/mini-media-player-bundle.js')
+(cd www && curl -OL 'https://raw.githubusercontent.com/thomasloven/lovelace-slider-entity-row/master/slider-entity-row.js')
 
