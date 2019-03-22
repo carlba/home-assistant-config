667eeb3fb336e5aaa377f7587f1aaee89a7ef365
Carl Bäckström <genzorg@gmail.com>
2019-03-22 06:58:38 +0100

    It is possible to test the TradfriMotionSensor with a test_hour parameter

--- appdaemon/apps/apps.py
+++ appdaemon/apps/apps.py
@@ -153,11 +153,12 @@ class TradfriMotionSensor(ExtendedHass):
         self.hours = self.args['hours'] or []
         self.turn_off_handle = None
         self.listen_state(self.handle_motion, self.binary_sensor)
+        self.test_hour = self.args.get('test_hour') or None
 
     def get_current_hour_light_settings(self):
         for hour_range in self.hours:
             # TODO: Should compare datetimes instead of hour values
-            range_match = hour_range['start'] <= datetime.now().hour <= hour_range['stop']
+            range_match = hour_range['start'] <= (self.test_hour or datetime.now().hour) <= hour_range['stop']
             self.log(f'{self.get_info()}: Matching: {hour_range["start"]} '
                      f'<= {datetime.now().hour} <= {hour_range["stop"]} = {range_match}')
             if range_match:
