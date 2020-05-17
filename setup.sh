b08e29331fac7e7c08c7d8f017f505617d3030b6
Carl Bäckström <genzorg@gmail.com>
2020-05-17 13:58:54 +0200

    Custom Cards
      - Xiaomi Vacuum card
      - Custom Header card (Makes header compact)

--- setup.sh
+++ setup.sh
@@ -9,8 +9,13 @@
 # https://github.com/custom-cards/circle-sensor-card
 (cd www && curl -OL 'https://raw.githubusercontent.com/custom-cards/circle-sensor-card/master/circle-sensor-card.js')
 
-[[ ! -d 'www/custom-lovelace/swipe-card' ]] && mkdir -p www/custom-lovelace/swipe-card
+# https://github.com/benct/lovelace-xiaomi-vacuum-card
+(cd www && curl -OL 'https://raw.githubusercontent.com/benct/lovelace-xiaomi-vacuum-card/master/xiaomi-vacuum-card.js')
 
+# https://maykar.github.io/custom-header/#installation/manual
+(cd www && curl -OL 'https://github.com/maykar/custom-header/releases/download/1.3.8/custom-header.js')
+
+[[ ! -d 'www/custom-lovelace/swipe-card' ]] && mkdir -p www/custom-lovelace/swipe-card
 (cd www/custom-lovelace/swipe-card && curl -OL 'https://raw.githubusercontent.com/bramkragten/custom-ui/master/swipe-card/swipe-card.js')
 
 # https://www.home-assistant.io/integrations/frontend
