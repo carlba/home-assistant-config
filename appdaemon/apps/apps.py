29e148784377f379abe49b3f456d40a250994d21
Carl Bäckström <genzorg@gmail.com>
2021-01-16 20:00:05 +0100

    Start using yapf for python formatting

--- appdaemon/apps/apps.py
+++ appdaemon/apps/apps.py
@@ -7,7 +7,6 @@ from datetime import datetime, time
 
 
 class ExtendedHass(hass.Hass):
-
     @staticmethod
     def get_info(level_in_stack: int = 1):
         target = inspect.stack()[level_in_stack][0]
@@ -74,7 +73,6 @@ class ExtendedHass(hass.Hass):
 
 # noinspection PyAttributeOutsideInit,PyUnusedLocal
 class Clean(ExtendedHass):
-
     def initialize(self):
         self.log(f"{self.__class__.__name__}.initialize")
         self.timer = 'timer.clean'
@@ -87,7 +85,8 @@ class Clean(ExtendedHass):
 
     def tracker(self, entity, attribute, old, new, kwargs):
         self.previous_state = old
-        self.log(f'Clean.tracker(): entity {entity}, old {old}, new {new}, triggered {self.triggered}')
+        self.log(
+            f'Clean.tracker(): entity {entity}, old {old}, new {new}, triggered {self.triggered}')
         if not self.triggered:
             if new == 'Clean':
                 self.start_cleaning()
@@ -106,7 +105,7 @@ class Clean(ExtendedHass):
         if self.previous_state != 'Clean':
             self.log(f'{self.get_info()}: previous_state{self.previous_state}')
             self.harmony_remote('remote.harmony_hub', 'turn_on', activity='Clean')
-        self.timer_start(self.timer, duration=75*60)
+        self.timer_start(self.timer, duration=75 * 60)
 
     def stop_cleaning(self, event_name, data, kwargs):
         self.log(f'{self.get_info()}: event_name {event_name}, data {data}')
@@ -116,7 +115,6 @@ class Clean(ExtendedHass):
 
 # noinspection PyAttributeOutsideInit,PyUnusedLocal
 class LogDeconzEvents(ExtendedHass):
-
     def initialize(self):
         self.log(f"{self.__class__.__name__}.initialize")
         self.listen_event(self.handle_event, "deconz_event")
@@ -127,7 +125,6 @@ class LogDeconzEvents(ExtendedHass):
 
 # noinspection PyAttributeOutsideInit,PyUnusedLocal
 class ColorTemperature(ExtendedHass):
-
     def initialize(self):
         self.log(f"{self.__class__.__name__}.initialize")
         self.daylight_sensor = self.args['sensor']
@@ -137,17 +134,22 @@ class ColorTemperature(ExtendedHass):
         # https://www.home-assistant.io/components/light/
         all_lights = self.get_state('group.all_lights', attribute='entity_id')
 
-        self.set_state('sensor.default_light_settings', state='on',
-                       attributes={'friendly_name': 'Default Light Settings', 'color_temp': '400'})
+        self.set_state('sensor.default_light_settings',
+                       state='on',
+                       attributes={
+                           'friendly_name': 'Default Light Settings',
+                           'color_temp': '400'
+                       })
 
     def handle_state(self, entity, attribute, old, new, kwargs):
-        self.log(f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
-                 f'kwargs: {repr(kwargs)}', level='INFO')
+        self.log(
+            f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
+            f'kwargs: {repr(kwargs)}',
+            level='INFO')
 
 
 # noinspection PyAttributeOutsideInit,PyUnusedLocal
 class TradfriMotionSensor(ExtendedHass):
-
     def initialize(self):
         self.log(f"{self.__class__.__name__}.initialize")
         self.binary_sensor = self.args['binary_sensor']
@@ -161,7 +163,8 @@ class TradfriMotionSensor(ExtendedHass):
     def get_current_hour_light_settings(self):
         for hour_range in self.hours:
             # TODO: Should compare datetimes instead of hour values
-            range_match = hour_range['start'] <= (self.test_hour or datetime.now().hour) <= hour_range['stop']
+            range_match = hour_range['start'] <= (self.test_hour
+                                                  or datetime.now().hour) <= hour_range['stop']
             self.log(f'{self.get_info()}: Matching: {hour_range["start"]} '
                      f'<= {datetime.now().hour} <= {hour_range["stop"]} = {range_match}')
             if range_match:
@@ -178,18 +181,22 @@ class TradfriMotionSensor(ExtendedHass):
         self.turn_off_handle = None
 
     def handle_motion(self, entity, attribute, old, new, kwargs):
-        self.log(f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
-                 f'kwargs: {repr(kwargs)}', level='INFO')
+        self.log(
+            f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
+            f'kwargs: {repr(kwargs)}',
+            level='INFO')
 
         current_hour_brightness, current_hour_duration = self.get_current_hour_light_settings()
 
         if (old == 'off' and new == 'on') and current_hour_brightness:
             self.log(f'Trying to set entity {self.light} to {current_hour_brightness} brightness')
-            self.turn_on(self.light,  brightness_pct=current_hour_brightness)
+            self.turn_on(self.light, brightness_pct=current_hour_brightness)
             self.log(f'Turned on {self.light}')
             if self.turn_off_handle:
                 self.turn_off_handle = self.cancel_timer(self.turn_off_handle)
-            self.turn_off_handle = self.run_in(self._turn_off, current_hour_duration, entity_id=entity)
+            self.turn_off_handle = self.run_in(self._turn_off,
+                                               current_hour_duration,
+                                               entity_id=entity)
 
         if old == 'on' and new == 'off':
             if not self.turn_off_handle:
@@ -199,15 +206,24 @@ class TradfriMotionSensor(ExtendedHass):
 
 # noinspection PyAttributeOutsideInit
 class RemoteControl(ExtendedHass):
-
     def initialize(self):
         self.log(f'{self.__class__.__name__}.initialize', level='INFO')
         self.id = self.args['id']
         self.listen_event(self.handle_event, self.args['event'], entity_id=self.id)
         self.type = self.args['type']
         self.event_map = {
-            'ikea': {'on': 1002, 'off': 2002},
-            'hue': {'on': 1001, 'off': 4001, 'up': 2002, 'down': 3002, 'play': 1002, 'pause': 4002}
+            'ikea': {
+                'on': 1002,
+                'off': 2002
+            },
+            'hue': {
+                'on': 1001,
+                'off': 4001,
+                'up': 2002,
+                'down': 3002,
+                'play': 1002,
+                'pause': 4002
+            }
         }
 
     def handle_event(self, event_name, data, kwargs):
@@ -222,16 +238,20 @@ class RemoteControl(ExtendedHass):
                 self.set_state(f'sensor.{data["id"]}', state='off')
                 self.handle_turn_off(event_name, data, kwargs)
 
-            elif self.event_map[self.type].get('up') and data['event'] == self.event_map[self.type]['up']:
+            elif self.event_map[self.type].get('up') and data['event'] == self.event_map[
+                    self.type]['up']:
                 self.handle_up(event_name, data, kwargs)
 
-            elif self.event_map[self.type].get('down') and data['event'] == self.event_map[self.type]['down']:
+            elif self.event_map[self.type].get('down') and data['event'] == self.event_map[
+                    self.type]['down']:
                 self.handle_down(event_name, data, kwargs)
 
-            elif self.event_map[self.type].get('play') and data['event'] == self.event_map[self.type]['play']:
+            elif self.event_map[self.type].get('play') and data['event'] == self.event_map[
+                    self.type]['play']:
                 self.handle_play(event_name, data, kwargs)
 
-            elif self.event_map[self.type].get('pause') and data['event'] == self.event_map[self.type]['pause']:
+            elif self.event_map[self.type].get('pause') and data['event'] == self.event_map[
+                    self.type]['pause']:
                 self.handle_pause(event_name, data, kwargs)
 
     def handle_turn_on(self, event_name, data, kwargs):
@@ -255,7 +275,6 @@ class RemoteControl(ExtendedHass):
 
 # noinspection PyAttributeOutsideInit
 class RemoteControlAction(RemoteControl):
-
     def initialize(self):
         super().initialize()
         self.entities = self.args['entities']
@@ -275,11 +294,14 @@ class RemoteControlAction(RemoteControl):
 
 # noinspection PyAttributeOutsideInit
 class RemoteControlService(RemoteControl):
-
     def initialize(self):
         super().initialize()
         self.entities: List[str] = self.args.get('entities')
-        self.service_calls: Dict[List[Dict[str, Union[str, Dict[str, Union[str, int]]]]]] = self.args['service_calls']
+        self.service_calls: Dict[List[Dict[str,
+                                           Union[str,
+                                                 Dict[str,
+                                                      Union[str,
+                                                            int]]]]]] = self.args['service_calls']
 
     def get_service_calls_for_event(self, service_calls, event: str):
         new_service_calls: List[dict] = []
@@ -297,10 +319,7 @@ class RemoteControlService(RemoteControl):
                     }
                     new_service_calls.append(service_call)
             else:
-                service_call = {
-                    'args': [call['service'].replace('.', '/')],
-                    'kwargs': call['data']
-                }
+                service_call = {'args': [call['service'].replace('.', '/')], 'kwargs': call['data']}
                 new_service_calls.append(service_call)
         return new_service_calls
 
@@ -315,13 +334,14 @@ class RemoteControlService(RemoteControl):
         super().handle_turn_off(event_name, data, kwargs)
         new_service_calls = self.get_service_calls_for_event(self.service_calls, 'off')
         for service_call in new_service_calls:
-            self.log(f'Trying to call service with args:{service_call["args"]} kwargs:{service_call["kwargs"]}')
+            self.log(
+                f'Trying to call service with args:{service_call["args"]} kwargs:{service_call["kwargs"]}'
+            )
             self.call_service(*service_call['args'], **service_call['kwargs'])
 
 
 # noinspection PyAttributeOutsideInit
 class MediaController(RemoteControl):
-
     def initialize(self):
         super().initialize()
         self.log(f'{self.__class__.__name__}.initialize', level='INFO')
@@ -331,8 +351,12 @@ class MediaController(RemoteControl):
         self.listen_state(self.handle_speaker_state, self.speaker)
 
         self.current_state = {
-            'tv': {self.tv: self.get_state(self.tv) or 'unknown'},
-            'speaker': {self.speaker: self.get_state(self.speaker) or 'unknown'}
+            'tv': {
+                self.tv: self.get_state(self.tv) or 'unknown'
+            },
+            'speaker': {
+                self.speaker: self.get_state(self.speaker) or 'unknown'
+            }
         }
 
     def handle_play(self, event_name, data, kwargs):
@@ -352,14 +376,18 @@ class MediaController(RemoteControl):
             self.call_service('media_player/media_pause', entity_id=self.speaker)
 
     def handle_tv_state(self, entity, attribute, old, new, kwargs):
-        self.log(f'entity: {entity}, attribute: {attribute}, old: {old}, '
-                 f'new {new}, kwargs: {repr(kwargs)}', level='INFO')
+        self.log(
+            f'entity: {entity}, attribute: {attribute}, old: {old}, '
+            f'new {new}, kwargs: {repr(kwargs)}',
+            level='INFO')
         self.current_state['tv'][entity] = new
         self.log(json.dumps(self.current_state))
 
     def handle_speaker_state(self, entity, attribute, old, new, kwargs):
-        self.log(f'entity: {entity}, attribute: {attribute}, old: {old}, '
-                 f'new {new}, kwargs: {repr(kwargs)}', level='INFO')
+        self.log(
+            f'entity: {entity}, attribute: {attribute}, old: {old}, '
+            f'new {new}, kwargs: {repr(kwargs)}',
+            level='INFO')
         self.current_state['speaker'][entity] = new
         self.log(self.current_state)
 
@@ -385,11 +413,13 @@ class MediaControllerVolume(MediaController):
 
         if self.current_state['tv'][self.tv] == 'playing':
             volume_level = self.get_state(self.tv, attribute="volume_level")
-            self.call_service('media_player/volume_set', volume_level=volume_level+0.1,
+            self.call_service('media_player/volume_set',
+                              volume_level=volume_level + 0.1,
                               entity_id=self.tv)
         elif self.current_state['speaker'][self.speaker] == 'playing':
             volume_level = self.get_state(self.speaker, attribute="volume_level")
-            self.call_service('media_player/volume_set', volume_level=volume_level+0.01,
+            self.call_service('media_player/volume_set',
+                              volume_level=volume_level + 0.01,
                               entity_id=self.speaker)
 
     def handle_down(self, event_name, data, kwargs):
@@ -399,13 +429,15 @@ class MediaControllerVolume(MediaController):
 
         if self.current_state['tv'][self.tv] == 'playing':
             volume_level = self.get_state(self.tv, attribute="volume_level")
-            new_volume_level = volume_level-0.1 if volume_level > 0.1 else 0
-            self.call_service('media_player/volume_set', volume_level=new_volume_level,
+            new_volume_level = volume_level - 0.1 if volume_level > 0.1 else 0
+            self.call_service('media_player/volume_set',
+                              volume_level=new_volume_level,
                               entity_id=self.tv)
         elif self.current_state['speaker'][self.speaker] == 'playing':
             volume_level = self.get_state(self.speaker, attribute="volume_level")
-            new_volume_level = volume_level-0.01 if volume_level > 0.01 else 0
-            self.call_service('media_player/volume_set', volume_level=new_volume_level,
+            new_volume_level = volume_level - 0.01 if volume_level > 0.01 else 0
+            self.call_service('media_player/volume_set',
+                              volume_level=new_volume_level,
                               entity_id=self.speaker)
 
     def handle_turn_on(self, event_name, data, kwargs):
@@ -422,7 +454,6 @@ class MediaControllerVolume(MediaController):
 
 # noinspection PyAttributeOutsideInit
 class TalkingTimer(ExtendedHass):
-
     def initialize(self):
         self.log(f'{self.__class__.__name__}.initialize', level='INFO')
         self.timer = self.args['timer']
@@ -437,8 +468,10 @@ class TalkingTimer(ExtendedHass):
         self.listen_event(self.stop_activity, event='timer.finished', entity_id=self.timer)
 
     def handle_entity_state(self, entity, attribute, old, new, kwargs):
-        self.log(f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
-                 f'kwargs: {repr(kwargs)}', level='INFO')
+        self.log(
+            f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
+            f'kwargs: {repr(kwargs)}',
+            level='INFO')
 
         if old == 'off' and new == 'on':
             self.run_in(self.read_information, self.information_delay)
@@ -453,17 +486,16 @@ class TalkingTimer(ExtendedHass):
     def read_information(self, _):
         if self.information_push:
             self.log(f'Trying to push {self.information} to Pushover')
-            self.call_service('notify/pushover',
-                              message=self.information)
+            self.call_service('notify/pushover', message=self.information)
 
-        if self.information_talk and (self.talk_hours[0] <= datetime.now().hour <= self.talk_hours[1]):
+        if self.information_talk and (self.talk_hours[0] <= datetime.now().hour <=
+                                      self.talk_hours[1]):
             self.log(f'Trying to say {self.information}')
             self.call_service('notify/gassistant', message=self.information)
 
 
 class TimeRange:
-
-    def __init__(self, data, time_format='%H:%M:%S', ):
+    def __init__(self, data, time_format='%H:%M:%S'):
         self.time_format = time_format
         self.time_range = {
             "start": self.time_to_timestring(data['start']),
@@ -490,7 +522,6 @@ class TimeRange:
 
 # noinspection PyAttributeOutsideInit
 class TimeBasedPersonTracker(ExtendedHass):
-
     def initialize(self):
         self.log(f'{self.__class__.__name__}.initialize', level='INFO')
         self.group = self.args['group']
@@ -504,8 +535,10 @@ class TimeBasedPersonTracker(ExtendedHass):
         return self.get_state('light').keys()
 
     def handle_group_state(self, entity, attribute, old, new, kwargs):
-        self.log(f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
-                 f'kwargs: {repr(kwargs)}', level='INFO')
+        self.log(
+            f'entity: {entity}, attribute: {attribute}, old: {old}, new {new}, '
+            f'kwargs: {repr(kwargs)}',
+            level='INFO')
 
         for time_range in self.time_ranges:
             if time_range.match_now():
